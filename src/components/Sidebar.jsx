40ab930e38de79f2707757fd7027c29e5939a43d
Kylian Enyegue <kylian.enyeguemvodo@gmail.com>
2026-05-19 20:59:49 +0200

    ajustement taille ecran

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,11 +10,13 @@ function Sidebar() {
     <nav className="navbar">
       <img src={keLogo} alt="KE" className="navbar-ke-logo" onClick={() => window.location.reload()} />
       <div className="navbar-links">
+        <img src={keLogo} alt="KE" className="navbar-ke-logo-mobile" onClick={() => window.location.reload()} />
         <button onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}><i className="fi fi-br-house-blank"></i> Home</button>
         <button onClick={() => scrollTo('about')}><i className="fi fi-rr-file-user"></i> About me</button>
         <button onClick={() => scrollTo('skills')}><i className="fi fi-ss-user-skill-gear"></i> Skills</button>
         <button onClick={() => scrollTo('projets')}><i className="fi fi-br-diagram-project"></i> Projets</button>
         <button onClick={() => scrollTo('contact')}><i className="fi fi-sr-phone-call"></i> Contact</button>
+        <a className="navbar-btn navbar-btn-mobile-cv" href="/CV.pdf" target="_blank" rel="noopener noreferrer">Mon CV</a>
       </div>
       <a className="navbar-btn" href="/CV.pdf" target="_blank" rel="noopener noreferrer">Mon CV</a>
     </nav>
